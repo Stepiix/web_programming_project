13ab4dbf89ad6523be6fc7568b22efdde3504657
jceso <01gabrielecesali@gmail.com>
2023-04-26 13:51:25 +0100

    Models

--- models/item.js
+++ models/item.js
@@ -2,7 +2,11 @@ var mongoose = require('mongoose');
 
 var ItemSchema = new mongoose.Schema({
     name: String,
-    quantity: Number
+    date: Date,
+    place_id: String,
+    details: String,
+    seated: Boolean,
+    e_structure: String  //If seated, ID of the eventStructure
 });
 
 module.exports = mongoose.model('Item', ItemSchema);
---
+++ models/ticket.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+var TicketSchema = new mongoose.Schema({
+    event_id: String,
+    type_id: String,
+    sale_id: String,
+});
+
+module.exports = mongoose.model('Ticket', TicketSchema);
