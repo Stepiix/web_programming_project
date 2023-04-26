1160f2523ad270c9bdff24e27d0c5dd4fdc02d24
jceso <01gabrielecesali@gmail.com>
2023-04-26 12:47:59 +0100

    Translated itemController

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -7,7 +7,7 @@ var itemController = {};
 itemController.showAll = function(req, res){
     Item.find({}).exec((err, dbitems)=>{
         if (err){
-            console.log('Erro a ler');
+            console.log('Reading error - Show the whole list');
             res.redirect('/error')
         } else {
             console.log(dbitems);
@@ -20,7 +20,7 @@ itemController.showAll = function(req, res){
 itemController.show = function(req, res){
     Item.findOne({_id:req.params.id}).exec((err, dbitem)=>{
         if (err){
-            console.log('Erro a ler');
+            console.log('Reading error - Find event to show');
             res.redirect('/error')
         } else {
             res.render('items/itemViewDetails', {item: dbitem});
@@ -38,7 +38,7 @@ itemController.create = function(req,res){
     var item = new Item(req.body);
     item.save((err)=>{
         if (err){
-            console.log('Erro a gravar');
+            console.log('Saving error - Create event');
             res.redirect('/error')
         } else {
             res.redirect('/items');
@@ -50,7 +50,7 @@ itemController.create = function(req,res){
 itemController.formEdit = function(req, res){
     Item.findOne({_id:req.params.id}).exec((err, dbitem)=>{
         if (err){
-            console.log('Erro a ler');
+            console.log('Reading error - Find event to edit');
             res.redirect('/error')
         } else {
             res.render('items/itemEditDetails', {item: dbitem});
@@ -62,7 +62,7 @@ itemController.formEdit = function(req, res){
 itemController.edit = function(req,res){
     Item.findByIdAndUpdate(req.body._id, req.body, (err, editedItem)=>{
         if (err){
-            console.log('Erro a gravar');
+            console.log('Saving error - Edit event');
             res.redirect('/error')
         } else {
             res.redirect('/items/show/'+req.body._id);
@@ -74,7 +74,7 @@ itemController.edit = function(req,res){
 itemController.delete = function(req, res){
     Item.remove({_id:req.params.id}).exec((err)=>{
         if (err){
-            console.log('Erro a ler');
+            console.log('Reading error - Find event to delete');
             
         } else {
             res.redirect('/items')
