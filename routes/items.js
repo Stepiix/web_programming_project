15fe049cf9dcfab2c7b1403d381f816bfa286b03
Stepan Barta <barta.stepi@gmail.com>
2023-04-27 15:27:36 +0100

    its not working now xd

--- routes/items.js
+++ routes/items.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var itemController = require('../controllers/itemController');
-var ticketController = require('../controllers/ticketController');
+var itemController = require('../controllers/userController');
 
 router.get('/', itemController.showAll ); //default
 router.get('/show/:id', itemController.show );
@@ -10,10 +9,4 @@ router.post('/create', itemController.create);
 router.get('/edit/:id', itemController.formEdit);
 router.post('/edit/:id', itemController.edit);
 router.get('/delete/:id', itemController.delete );
-
-router.get('/:id_e/tickets', ticketController.showAll );
-router.get('/:id_e/tickets/create', ticketController.formCreate);
-router.post('/:id_e/tickets/create', ticketController.create);
-router.get('/:id_e/tickets/delete/:id', ticketController.delete );
-  
 module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var itemController = require('../controllers/userController');
+var ticketController = require('../controllers/ticketController');
+
+router.get('/', itemController.showAll ); //default
+router.get('/show/:id', itemController.show );
+router.get('/create', itemController.formCreate);
+router.post('/create', itemController.create);
+router.get('/edit/:id', itemController.formEdit);
+router.post('/edit/:id', itemController.edit);
+router.get('/delete/:id', itemController.delete );
+
+router.get('/:id_e/tickets', ticketController.showAll );
+router.get('/:id_e/tickets/create', ticketController.formCreate);
+router.post('/:id_e/tickets/create', ticketController.create);
+router.get('/:id_e/tickets/delete/:id', ticketController.delete );
+  
+module.exports = router;
