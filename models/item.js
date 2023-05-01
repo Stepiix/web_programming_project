1237cfe13b55ce9b114bce386e325a14f1567ff4
jceso <01gabrielecesali@gmail.com>
2023-05-01 17:25:42 +0100

    Prettier logo

--- models/item.js
+++ models/item.js
@@ -7,14 +7,8 @@ var ItemSchema = new mongoose.Schema({
     place_id: String,
     details: String,
     capacity: Number,
-<<<<<<< HEAD
-    seated: Boolean,
-    e_structure: String,  //If seated, ID of the eventStructure
-    image: File
-=======
     seated: { type: String, enum : ['on', 'off'] },
     e_structure: String  //If seated, ID of the eventStructure
->>>>>>> 036e14fc747c86874a5dae35e8a594d1ff205f99
 });
 
 module.exports = mongoose.model('Item', ItemSchema);
