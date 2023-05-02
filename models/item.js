cdd28c587c35879b191b8ac79e5ccc676d49b976
Stepan Barta <barta.stepi@gmail.com>
2023-05-02 10:35:34 +0100

    blabla

--- models/item.js
+++ models/item.js
@@ -7,11 +7,8 @@ var ItemSchema = new mongoose.Schema({
     place_id: String,
     details: String,
     capacity: Number,
-<<<<<<< HEAD
     seated: Boolean,
-=======
     seated: { type: String, enum : ['on', 'off'] },
->>>>>>> 1237cfe13b55ce9b114bce386e325a14f1567ff4
     e_structure: String  //If seated, ID of the eventStructure
 });
 
