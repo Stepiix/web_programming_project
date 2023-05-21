ecd10df24099f56e1f80e10b0df3a9f329394b5a
Stepan Barta <barta.stepi@gmail.com>
2023-05-21 17:07:22 +0100

    Our website is sick!!

---
+++ project/2milestone/src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { SignupComponent } from './signup/signup.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AlleventsComponent } from './allevents/allevents.component';
+import { MycartComponent } from './mycart/mycart.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'allevents', component: AlleventsComponent},
+  { path: 'mycart', component: MycartComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'signup', component: SignupComponent},
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- project/2milestone/src/app/app.module.ts
+++ project/2milestone/src/app/app.module.ts
@@ -2,13 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { AlleventsComponent } from './allevents/allevents.component';
+import { MycartComponent } from './mycart/mycart.component';
+import { RegisterComponent } from './register/register.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AlleventsComponent,
+    MycartComponent,
+    RegisterComponent,
+    SignupComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
