d8bddd0213546c9436646de63051a3ed0aaab305
Stepan Barta <barta.stepi@gmail.com>
2023-05-30 13:29:21 +0100

    wow

--- project/2milestone/src/app/services/auth/auth-service.service.ts
+++ project/2milestone/src/app/services/auth/auth-service.service.ts
@@ -1,9 +1,21 @@
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+const endpoint = "http://localhost:3000/users/";
+const options = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createUser(user: User): Observable<any> {
+    return this.http.post<any>(endpoint + "create2", user, options);
+  }
+
 }
