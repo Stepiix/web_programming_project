8f670b860f6b4fb7648fcba75da4ac103dfc1311
jceso <01gabrielecesali@gmail.com>
2023-05-31 11:12:20 +0100

    Places insteas of IDs

--- project/2milestone/src/app/allevents/allevents.component.ts
+++ project/2milestone/src/app/allevents/allevents.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PlaceService } from '../services/event/place.service';
 import { EventServiceService } from '../services/event/event-service.service';
+import { place } from '../models/place';
 import { eventPlace } from "../models/event";
 
 @Component({
@@ -8,18 +10,40 @@ import { eventPlace } from "../models/event";
   styleUrls: ['./allevents.component.css']
 })
 export class AlleventsComponent implements OnInit{
-
+  listOfPlaces: place[] = [];
   listOfEvents: eventPlace[] = [];
 
-  constructor(private eventService: EventServiceService){}
+  constructor(private eventService: EventServiceService, private placeService: PlaceService){}
   ngOnInit(): void {
+    this.getAllPlaces();
     this.getAllEvents();
+  }
+
+  getAllPlaces(){
+    this.placeService.getAllPlaces().subscribe({
+      next: (data)=>{
+        this.listOfPlaces = data;
+      },
+      error: err=>{
+
+      }
+    })
   }
 
   getAllEvents(){
     this.eventService.getAllEvents().subscribe({
       next: (data)=>{
         this.listOfEvents = data;
+
+        for (let i=0; this.listOfEvents[i]!= null; i++) {
+          this.listOfEvents[i].date = this.listOfEvents[i].date.split("T", 2)[0]
+
+          for (let j=0; this.listOfPlaces[j]!= null; j++) {
+            if (this.listOfEvents[i].place_id == this.listOfPlaces[j]._id){
+              this.listOfEvents[i].place_id = this.listOfPlaces[j].name
+            }
+          }
+        }
       },
       error: err=>{
 
---
+++ project/2milestone/src/app/models/place.ts
@@ -0,0 +1,4 @@
+export class place{
+    _id: string = "";
+    name: string = "";
+}
